225dd46b9ae842aab121d133d7877fa76dfd49ff
warforged5 <148923937+warforged5@users.noreply.github.com>
2024-08-13 07:24:16 -0400

    Add files via upload

---
+++ Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kirstenfournier_1/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kirstenfournier_1/Documents/wheeler_nhs/wheeler_nhs"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/kirstenfournier_1/Documents/wheeler_nhs/wheeler_nhs/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.4.3"
+export "FLUTTER_BUILD_NUMBER=8"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMzVkYjkxMWJhMjc5NzIyZjVlNjg1ZjM4YjBlZDMwZmE3ZTg1NzBhLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/kirstenfournier_1/Documents/wheeler_nhs/wheeler_nhs/.dart_tool/package_config.json"
